b280169af617e1f8f1e6ba276af0f2a2457a45b6
Valentin Bandelier <vmbandelier@gmail.com>
2018-06-04 13:55:01 +0200

    DB management

--- app-test.py
+++ app-test.py
@@ -1,15 +1,16 @@
 import unittest
 import os
-import tempfile
 import json
 
-import app
+from app import app, db
+
+TEST_DB = 'test.db'
 
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
         """Initial test: Ensure flask was set up correctly."""
-        tester = app.app.test_client(self)
+        tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
@@ -21,15 +22,15 @@ class BasicTestCase(unittest.TestCase):
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         """Set up a blank temp database before each test."""
-        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
-        app.app.config['TESTING'] = True
-        self.app = app.app.test_client()
-        app.init_db()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.creat_all()
 
     def tearDown(self):
         """Destroy blank temp database after each test."""
-        os.close(self.db_fd)
-        os.unlink(app.app.config['DATABASE'])
+        db.drop_all()
 
     def login(self, username, password):
         """Login helper function."""
@@ -46,36 +47,36 @@ class FlaskTestCase(unittest.TestCase):
     def test_empty_db(self):
         """Ensure database is blank."""
         rv = self.app.get('/')
-        assert b'No entries yet. Add some!' in rv.data
+        self.assertIn(b'No entries yet. Add some!', rv.data)
 
     def test_login_logout(self):
         """Test login and logout using helper functions."""
         rv = self.login(
-            app.app.config['USERNAME'],
-            app.app.config['PASSWORD']
+            app.config['USERNAME'],
+            app.config['PASSWORD']
         )
-        assert b'You were logged in' in rv.data
+        self.assertIn(b'You were logged in', rv.data)
         rv = self.logout()
-        assert b'You were logged out' in rv.data
+        self.assertIn(b'You were logged out', rv.data)
         rv = self.login(
-            app.app.config['USERNAME'],
-            app.app.config['PASSWORD'] + 'x'
+            app.config['USERNAME'],
+            app.config['PASSWORD'] + 'x'
         )
-        assert b'Invalid password' in rv.data
+        self.assertIn(b'Invalid password', rv.data)
 
     def test_messages(self):
         """Ensure that a user can post messages."""
         self.login(
-            app.app.config['USERNAME'],
-            app.app.config['PASSWORD']
+            app.config['USERNAME'],
+            app.config['PASSWORD']
         )
         rv = self.app.post('/add', data=dict(
             title='<Hello>',
             text='<strong>HTML</strong> allowed here'
         ), follow_redirects=True)
-        assert b'No entries yet. Add some!' not in rv.data
-        assert b'&lt;Hello&gt;' in rv.data
-        assert b'<strong>HTML</strong> allowed here' in rv.data
+        self.assertNotIn(b'No entries yet. Add some!', rv.data)
+        self.assertIn(b'&lt;Hello&gt;', rv.data)
+        self.assertIn(b'<strong>HTML</strong> allowed here', rv.data)
 
     def test_delete_message(self):
         """Ensure the messages are being deleted."""
--- app.py
+++ app.py
@@ -1,53 +1,50 @@
 # imports
+import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# get the folder where this file runs
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # configuration
-DATABASE = 'flask.db'
+DATABASE = 'flaskr.db'
 DEBUG = True
-SECRET_KEY = 'maria-scotty-valentin'
+SECRET_KEY = 'my_precious'
 USERNAME = 'admin'
 PASSWORD = 'admin'
+
+# define the full path for the datebase
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# database config
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # create and initialize app
 app = Flask(__name__)
 app.config.from_object(__name__)
+db = SQLAlchemy(app)
 
-# connect to database
-def connect_db():
-    """Connexts to the database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-# create the datebase
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-# open database connection
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-# close database connection
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+from models import Flaskr
 
 @app.route('/')
 def index():
     """Searches the database for entries, then plisplays them."""
-    db = get_db()
-    cur = db.execute('select * from entries order by id desc')
-    entries = cur.fetchall()
+    entries = Flaskr.query.all()
     return render_template('index.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    new_entry = Flaskr(request.form['title'], request.form['text'])
+    db.session.add(new_entry)
+    db.session.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -71,32 +68,17 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('index'))
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    """Add new post to database."""
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute(
-        'insert into entries (title, text) values (?, ?)',
-        [request.form['title'], request.form['text']]
-    )
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('index'))
-
 @app.route('/delete/<post_id>', methods=['GET'])
 def delete_entry(post_id):
     """Delete post from database"""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db = get_db()
-        db.execute('delete from entries where id=' + post_id)
-        db.commit()
+        Flaskr.query.filter_by(post_id=post_id).delete()
+        db.session.commit()
         result = {'status': 1, 'message': 'Post Deleted'}
+        flash('The entry was deleted.')
     except Exception as e:
         result = {'status': 0, 'message': repr(e)}
-
     return jsonify(result)
 
 if __name__ == '__main__':
